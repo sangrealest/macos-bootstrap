3d3a2f35a356f845e75e4eb68afaa1e245e13a4c
Shanker Lee <shanker@yeah.net>
2019-02-26 20:01:30 +0800

    init commit

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash -e
+#
+# Sets up requirements to provision with ansible
+#
+
+if [ ! $(which pip) ]
+then
+    sudo easy_install pip
+fi
+
+sudo pip install --upgrade pip
+
+echo "Installing Ansible"
+sudo pip install --upgrade ansible 
